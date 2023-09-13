9d4903b0bbaf4c84a6881dbf3a207c96d0ca65e2
YuriOliveira <yuri.oliveira@opensingular.com>
2023-09-13 17:06:37 -0300

    commit

--- src/main/java/org/opensingular/hackathon/view/util/JQueryMaskBehaviour.java
+++ src/main/java/org/opensingular/hackathon/view/util/JQueryMaskBehaviour.java
@@ -1,51 +1,30 @@
 package org.opensingular.hackathon.view.util;
 
-import org.apache.wicket.Application;
 import org.apache.wicket.Component;
-import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.head.*;
-import org.apache.wicket.request.Url;
-import org.apache.wicket.request.resource.ResourceReference;
-import org.apache.wicket.request.resource.UrlResourceReference;
-import org.intellij.lang.annotations.Language;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class JQueryMaskBehaviour extends Behavior {
+public class JQueryMaskBehaviour extends JQueryPluginBehavior {
 
     public static final String MASK_MIN_URL = "https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js";
-    @Language("js")
     private String mask;
 
-    public JQueryMaskBehaviour(@Language("js") String mask) {
+    public JQueryMaskBehaviour(String mask) {
         this.mask = mask;
     }
 
     @Override
-    public void renderHead(Component component, IHeaderResponse response) {
-        response.render(JavaScriptReferenceHeaderItem.forUrl(MASK_MIN_URL));
-        response.render(OnDomReadyHeaderItem.forScript(getMaskScript(component.getMarkupId())));
+    protected List<String> onDomReadyScript(Component component) {
+        return List.of(getMaskScript(component.getMarkupId()));
     }
 
-    @NotNull
-    private static UrlResourceReference getMaskPluginReference() {
-        return new UrlResourceReference(Url.parse(MASK_MIN_URL)) {
-            @Override
-            public List<HeaderItem> getDependencies() {
-                Application application = Application.get();
-                ResourceReference jqueryRef = application.getJavaScriptLibrarySettings().
-                        getJQueryReference();
-
-                return List.of(JavaScriptHeaderItem.forReference(jqueryRef));
-            }
-        };
+    @Override
+    protected List<String> getPluginScripts() {
+        return List.of(MASK_MIN_URL);
     }
 
-    @Language("js")
-    public String getMaskScript(@Language("js") String componentId) {
+    public String getMaskScript(String componentId) {
         return " $('#" + componentId + "').mask('" + mask + "');";
     }
-
 
 }
