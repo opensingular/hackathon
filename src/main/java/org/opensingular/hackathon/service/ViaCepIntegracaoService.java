2ea18c488d48fb4e320f789267920718f692200f
Luiz-GL-Campos <luiz1gustavocampos@gmail.com>
2023-09-16 16:23:35 -0300

    Desafio 1

--- src/main/java/org/opensingular/hackathon/service/ViaCepIntegracaoService.java
+++ src/main/java/org/opensingular/hackathon/service/ViaCepIntegracaoService.java
@@ -9,12 +9,14 @@ public class ViaCepIntegracaoService {
 
     /**
      * <p>
+     * viacep.com.br/ws/01001000/json/
      * Preencher utilizando o serviço do <a href="https://viacep.com.br/">Via Cep</a>
      *
      * <p>
      * Utilizar a classe {@link RestTemplate} do SpringFramework
      */
     public EnderecoDTO buscar(String cep) {
+
         return new EnderecoDTO();
     }
 }
