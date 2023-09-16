e06674803ec9c24380c24cc4a331de931eb05c8a
TESTE <augus96ribeiro@gmail.com>
2023-09-16 17:13:02 -0300

    First commit

--- src/main/java/org/opensingular/hackathon/service/ViaCepIntegracaoService.java
+++ src/main/java/org/opensingular/hackathon/service/ViaCepIntegracaoService.java
@@ -1,6 +1,10 @@
 package org.opensingular.hackathon.service;
 
 import org.opensingular.hackathon.model.EnderecoDTO;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +19,10 @@ public class ViaCepIntegracaoService {
      * Utilizar a classe {@link RestTemplate} do SpringFramework
      */
     public EnderecoDTO buscar(String cep) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ViaCepIntegracaoService> responseEntity = restTemplate.postForEntity("viacep.com.br/ws/01001000/json/", cep, ViaCepIntegracaoService.class);
         return new EnderecoDTO();
     }
 }
+   
